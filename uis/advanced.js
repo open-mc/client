1722a9612c999ad4af968a0aed810c38ec1b57aa
BlobKat <blob.kat@hotmail.com>
2023-02-06 13:25:52 +0000

    speed

--- uis/advanced.js
+++ uis/advanced.js
@@ -2,10 +2,10 @@ import { options } from "../save.js"
 import { Btn, Label, Scale, showUI, Spacer, UI } from "../ui.js"
 import { optionsScreen } from "./options.js"
 
-function speedChange(a = options.speed/2){
-	if(a > 0.49 && a < 0.51)a = 0.5
-	options.speed = a*2
-	return [a > 0.005 ? 'Speed: '+(Math.round(a*200)/100).toFixed(2)+'x' : 'Speed: Paused', a]
+function speedChange(a = options.speed/4){
+	if(a > 0.24 && a < 0.26)a = 0.5
+	options.speed = a*4
+	return [a > 0.01 ? 'Speed: '+(Math.round(a*200)/100).toFixed(2)+'x' : 'Speed: Paused', a]
 }
 let resetBtn
 const ui = UI('menu',
