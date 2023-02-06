e395bab236f17dd89e189462d74ba3daca598d20
BlobKat <blob.kat@hotmail.com>
2023-02-06 13:33:55 +0000

    speed fix 2

--- uis/advanced.js
+++ uis/advanced.js
@@ -3,7 +3,7 @@ import { Btn, Label, Scale, showUI, Spacer, UI } from "../ui.js"
 import { optionsScreen } from "./options.js"
 
 function speedChange(a = options.speed/4){
-	if(a > 0.24 && a < 0.26)a = 0.5
+	if(a > 0.24 && a < 0.26)a = 0.25
 	options.speed = a*4
 	return [a > 0.01 ? 'Speed: '+(Math.round(a*400)/100).toFixed(2)+'x' : 'Speed: Paused', a]
 }
