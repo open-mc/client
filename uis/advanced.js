ad04c5c38c4a0488ac271aa4ac4eb4a2d7571796
BlobKat <blob.kat@hotmail.com>
2023-02-06 13:32:16 +0000

    speed fix

--- uis/advanced.js
+++ uis/advanced.js
@@ -5,7 +5,7 @@ import { optionsScreen } from "./options.js"
 function speedChange(a = options.speed/4){
 	if(a > 0.24 && a < 0.26)a = 0.5
 	options.speed = a*4
-	return [a > 0.01 ? 'Speed: '+(Math.round(a*200)/100).toFixed(2)+'x' : 'Speed: Paused', a]
+	return [a > 0.01 ? 'Speed: '+(Math.round(a*400)/100).toFixed(2)+'x' : 'Speed: Paused', a]
 }
 let resetBtn
 const ui = UI('menu',
