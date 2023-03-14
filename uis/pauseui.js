9a39715280924321df3bd62fa0146aaa9e6ad668
BlobKat <blob.kat@hotmail.com>
2023-03-14 08:28:04 +0000

    repo change

--- uis/pauseui.js
+++ uis/pauseui.js
@@ -9,9 +9,9 @@ const pauseui = UI('menu',
 	Label('Game menu'),
 	Btn('Back to Game', hideUI),
 	Row(Btn('Options', optionsScreen), Btn('Feedback', feedback)),
-	Row(Btn('Controls', controlsScreen), Btn('Credits', () => open('https://github.com/openmc2d/client/blob/preview/CREDITS.md','_blank'))),
+	Row(Btn('Controls', controlsScreen), Btn('Credits', () => open('https://github.com/open-mc/client/blob/preview/CREDITS.md','_blank'))),
 	Btn('Disconnect', serverlist),
-	Div('', Label('Changelog').attr('style', 'height:unset;line-height:unset').attr('onclick', "window.open('https://github.com/openmc2d/client/blob/main/CHANGELOG.md','_blank')")).attr('style', 'position: absolute; bottom: 2rem; display: flex; right: 2rem; text-decoration: underline; opacity: 0.4; font-size: 7rem; cursor: pointer')
+	Div('', Label('Changelog').attr('style', 'height:unset;line-height:unset').attr('onclick', "window.open('https://github.com/open-mc/client/blob/main/CHANGELOG.md','_blank')")).attr('style', 'position: absolute; bottom: 2rem; display: flex; right: 2rem; text-decoration: underline; opacity: 0.4; font-size: 7rem; cursor: pointer')
 )
 pauseui.esc = hideUI
 
