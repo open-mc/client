a35424a88c1c768ed3f637fd15ddc5be26eff31e
BlobTheKat <blob.kat@hotmail.com>
2024-06-03 15:46:45 +0100

    maybe this will work

--- iframe/index.js
+++ iframe/index.js
@@ -1,9 +1,9 @@
 import { playerControls } from './controls.js'
 import { DataWriter } from '/server/modules/dataproto.js'
 import { mePhysics, stepEntity } from './entity.js'
-import { gridEventMap, getblock, entityMap, map, cam, onPlayerLoad, world, bigintOffset, me, genLightmap, lightTint, lightTex, lightArr, setGamma, getTint, getLightValue, W2, H2, SCALE, toBlockExact } from 'world'
+import { getblock, entityMap, map, cam, onPlayerLoad, world, me, W2, H2, SCALE } from 'world'
 import * as pointer from './pointer.js'
-import { onKey, drawLayer, options, paused, _renderPhases, renderBoxes, renderF3, send, download, copy, pause, _updatePaused, drawText, calcText, textShadeCol, _networkUsage, networkUsage, listen, _tickPhases, renderUI, _cbs, onmousemove, _joypadMoveCbs, onwheel, _optionListeners, _setChatFocused, _onvoice, voice, _onPacket, onfocus, onblur } from 'api'
+import { options, paused, _renderPhases, renderBoxes, send, download, copy, pause, _updatePaused, drawText, calcText, _networkUsage, listen, _tickPhases, renderUI, _cbs, onmousemove, _joypadMoveCbs, onwheel, _optionListeners, _setChatFocused, _onvoice, voice, _onPacket, onfocus, onblur } from 'api'
 import { _recalcDimensions, Blocks, Items, Entities, BlockIDs, ItemIDs, EntityIDs, Block, Item, Entity, Classes } from 'definitions'
 import { jsonToType } from '/server/modules/dataproto.js'
 import { onChat } from './chat.js'
@@ -66,7 +66,7 @@ const { CAMERA_DYNAMIC, CAMERA_FOLLOW_SMOOTH, CAMERA_FOLLOW_POINTER,
 let flashbang = 0
 let frames = 0
 globalThis.fps = 0
-globalThis.frame = () => {
+export const frame = () => {
 	const correctT = t
 	t *= options.speed; dt *= options.speed
 	_networkUsage()
