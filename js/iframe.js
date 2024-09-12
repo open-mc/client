415a07432973852d3f32b538b38d3d7eb0b0aae2
BlobTheKat <blob.kat@hotmail.com>
2024-09-12 19:01:39 +0100

    err

--- js/iframe.js
+++ js/iframe.js
@@ -14,8 +14,10 @@ if(!HTMLScriptElement.supports('importmap')){
 
 export let iframe = document.createElement('iframe'), win = null
 iframe.srcdoc = `<html style="height:100%;cursor:pointer"><style></style>${shimScript}<script>addEventListener('message',e=>{
-let E="data:application/javascript,export%20default%20",H=${JSON.stringify(location.origin+'/')},[d,f,F]=e.data,m={__proto__:null},R=(b,s,c=s.charCodeAt(0))=>c==47||(c==46&&((c=s.charCodeAt(1))==47||(c==46&&s.charCodeAt(2)==47)))?new URL(s,b).href:s.startsWith(H)?'data:application/javascript,':c==c?s:b;(globalThis.__import__=(b,s='')=>import(R(b,s))).meta=u=>m[u]??=Object.freeze({url:u,resolve:s=>R(u,s)});for(let{0:k,1:v}of __import__.map=d){
-if(!v){m[k]='data:application/javascript,';continue};if(v.type=='application/javascript'){m[k]=URL.createObjectURL(v);continue}
+onerror=(_,f,l,c,e)=>parent.postMessage([e,__import__.rmap.get(f)??f,l,c],'*')
+onunhandledrejection=e=>parent.postMessage([e.reason,'',0,0],'*')
+let E="data:application/javascript,export%20default%20",H=${JSON.stringify(location.origin+'/')},[d,f,F]=e.data,m={__proto__:null},R=(b,s,c=s.charCodeAt(0))=>c==47||(c==46&&((c=s.charCodeAt(1))==47||(c==46&&s.charCodeAt(2)==47)))?new URL(s,b).href:s.startsWith(H)?'data:application/javascript,':c==c?s:b;(globalThis.__import__=(b,s='')=>import(R(b,s))).meta=u=>m[u]??=Object.freeze({url:u,resolve:s=>R(u,s)});__import__.rmap=new Map;for(let{0:k,1:v}of __import__.map=d){
+if(!v){m[k]='data:application/javascript,';continue};if(v.type=='application/javascript'){__import__.rmap.set(m[k]=URL.createObjectURL(v),k);continue}
 let c=v.type,R="__import__.map.get("+encodeURI(JSON.stringify(k))+")"
 if(c.startsWith('image/'))m[k]=E+"Img("+R+")"
 else if(c=='application/json')m[k]=E+"await%20new%20Response("+R+").json()"
@@ -90,8 +92,10 @@ export class LocalSocket extends MessageChannel{
 		ifr.sandbox = 'allow-scripts allow-downloads'
 		ifr.allow = 'cross-origin-isolated'
 		ifr.srcdoc = `<script>addEventListener('message',e=>{
-let E="data:application/javascript,export%20default%20",H=${JSON.stringify(location.origin+'/')},d=(globalThis.__data__=e.data).cache,m={__proto__:null},R=(b,s,c=s.charCodeAt(0))=>c==47||(c==46&&((c=s.charCodeAt(1))==47||(c==46&&s.charCodeAt(2)==47)))?new URL(s,b).href:s.startsWith(H)?'data:application/javascript,':c==c?s:b;(globalThis.__import__=(b,s='')=>import(R(b,s))).meta=u=>m[u]??=Object.freeze({url:u,resolve:s=>R(u,s)});__import__.origin=H;for(let{0:k,1:v}of __import__.map=d){
-if(!v){m[k]='data:application/javascript,';continue};if(v.type=='application/javascript'){m[k]=URL.createObjectURL(v);continue}}
+onerror=(_,f,l,c,e)=>parent.postMessage([e,__import__.rmap.get(f)??f,l,c],'*')
+onunhandledrejection=e=>parent.postMessage([e.reason,'',0,0],'*')
+let E="data:application/javascript,export%20default%20",H=${JSON.stringify(location.origin+'/')},d=(globalThis.__data__=e.data).cache,m={__proto__:null},R=(b,s,c=s.charCodeAt(0))=>c==47||(c==46&&((c=s.charCodeAt(1))==47||(c==46&&s.charCodeAt(2)==47)))?new URL(s,b).href:s.startsWith(H)?'data:application/javascript,':c==c?s:b;(globalThis.__import__=(b,s='')=>import(R(b,s))).meta=u=>m[u]??=Object.freeze({url:u,resolve:s=>R(u,s)});__import__.origin=H;__import__.rmap=new Map;for(let{0:k,1:v}of __import__.map=d){
+if(!v){m[k]='data:application/javascript,';continue};if(v.type=='application/javascript'){__import__.rmap.set(m[k]=URL.createObjectURL(v),k);continue}}
 d=document.createElement('script');d.type='importmap';d.textContent=JSON.stringify({imports:m});document.head.append(d);m={__proto__:null}
 import(H+'localserver/index.js')
 globalThis.parentPort = null
@@ -200,6 +204,7 @@ onmessage = ({data, source}) => {
 		}
 		return
 	}
+	
 	if(data instanceof ArrayBuffer || typeof data == 'string') ws?.send(data)
 	if(typeof data != 'object'){
 		if(data === true) pause()
@@ -213,7 +218,7 @@ onmessage = ({data, source}) => {
 	if(data instanceof Blob) download(data)
 	else if(Array.isArray(data)){
 		if(data.length == 1 && data[0] instanceof Blob) navigator.clipboard.write([new ClipboardItem({[data[0].type]: data[0]})])
-		else; //onerror(undefined,''+data[1],+data[2],+data[3],data[0])
+		else onerror(undefined,''+data[1],+data[2],+data[3],data[0])
 	}
 }
 const a = document.createElement('a')
